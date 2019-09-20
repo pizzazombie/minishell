8438c9da3447d17cb518a79c89cd66622e0d5ac3
Mabelle Kami <mkami@mi-j4.21-school.ru>
2019-09-20 19:48:12 +0300

    cd

--- commands.c
+++ commands.c
@@ -11,67 +11,7 @@
 /* ************************************************************************** */
 
 #include "shell.h"
-void	ft_cd_to_path(char *path, t_shell *shell)
-{
-	char	*here;
-	char *temp;
-	char	buff[4001];
 
-	here = getcwd(buff, 4000);
-	if (chdir(path) == 0)
-	{
-		temp = ft_strjoin("setenv OLDPWD ", here);
-		ft_setenv(shell, temp);
-		free(shell->oldpwd);
-		shell->oldpwd = ft_strdup(here);
-		free(temp);
-		temp = ft_strjoin("setenv PWD ", path);
-		ft_setenv(shell, temp);
-		free(temp);
-		free(shell->pwd);
-		shell->pwd = ft_strdup(path);
-
-	}
-	else
-	{
-		ft_printf("%s: No such file or directory.\n", path);
-	}
-	
-}
-void	ft_cd(char *str, t_shell *shell)
-{
-	char **args;
-	char *path;
-	char *oldpath;
-
-	args = ft_strsplit_wide(str, ' ');
-	//path = get_env_var(shell->env, "PWD");
-	//oldpath = get_env_var(shell->env, "PWD");
-	if (args[0][2] != '\0')
-	{
-		ft_putstr("minishell: command not found: ");
-		ft_putendl(args[0]);
-		ft_free_mas(args);
-		return ;
-	}
-	else if (args[1] != 0 && args[2] != 0)
-	{
-		ft_putendl("cd: Too many arguments.");
-		ft_free_mas(args);
-		return ;
-	}
-	else if (args[1] == 0 || (ft_strncmp(args[1], "--", 2) == 0 && args[1][2] == '\0') || (ft_strncmp(args[1], "~", 1) == 0 && args[1][1] == '\0'))
-	{
-		ft_cd_to_path(shell->home, shell);
-	}
-	else if (ft_strncmp(args[1], "-", 1) == 0 && args[1][1] == '\0')
-		ft_cd_to_path(shell->oldpwd, shell);
-	else
-	{
-		ft_cd_to_path(args[1], shell);
-	}
-	//ft_putchar('\n');
-}
 int ft_str_len_without_br(char *str)
 {
 	int i;
